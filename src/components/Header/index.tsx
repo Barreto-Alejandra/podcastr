e002415cab9e928aa92051ce6f64bd38b3b82060
Alejandra <alejandra.barreto.ab@gmail.com>
2021-04-21 21:47:55 -0500

    added header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,11 @@
 export function Header() {
     return(
         <header>
-            
+            <img src="./logo.svg" alt="Podcastr"/>
+
+            <p>O melhor para você ouvir, sempre</p>
+
+            <span>Qui, 8 Abril</span>
         </header>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
+import { Header } from "../components/Header";
 
 export default function Home() {
   return (
-    <h1>Hellooo</h1>
+    <Header />
   )
 }
